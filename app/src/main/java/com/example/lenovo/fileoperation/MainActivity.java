599682d9cbf5037aa03ed4a4c98cd10b752c7a38
hym <1349456160@qq.com>
2017-09-08 13:56:51 +0800

    实现了信息的存入和读取。 仅能实现一次信息的存入，再次存入信息时会覆盖之前的信息。

--- app/src/main/java/com/example/lenovo/fileoperation/MainActivity.java
+++ app/src/main/java/com/example/lenovo/fileoperation/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.lenovo.fileoperation;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText text1,text2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+        Button button= (Button) findViewById(R.id.button);
+        Button button1=(Button) findViewById(R.id.restore_button);
+         text1=(EditText) findViewById(R.id.text1);
+         text2= (EditText) findViewById(R.id.text2);
+        button.setOnClickListener(this);
+        button1.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        String name=text2.getText().toString();
+        String number=text1.getText().toString();
+        switch (view.getId()){
+            case R.id.button:
+                SharedPreferences.Editor editor
+                        =getSharedPreferences("data",MODE_PRIVATE).edit();
+                editor.putString("学号",number);
+                editor.putString("姓名",name);
+                editor.apply();
+                break;
+            case R.id.restore_button:
+                SharedPreferences pref
+                        =getSharedPreferences("data",MODE_PRIVATE);
+                String names=pref.getString("姓名","无");
+                String numbers=pref.getString("学号","无");
+                String s="姓名："+names+"学号："+numbers;
+                Toast.makeText(MainActivity.this,s,Toast.LENGTH_SHORT).show();
+            default:
+                break;
+        }
+    }
+
+
 }
