7f116c8bf5c2554d0473927f7ecfa619eeca3625
hym <1349456160@qq.com>
2017-09-11 14:50:51 +0800

    将信息读入读出方式改为文件读取方式

--- app/src/main/java/com/example/lenovo/fileoperation/MainActivity.java
+++ app/src/main/java/com/example/lenovo/fileoperation/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lenovo.fileoperation;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     EditText text1,text2;
@@ -25,25 +34,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        String name=text2.getText().toString();
-        String number=text1.getText().toString();
         switch (view.getId()){
             case R.id.button:
-                SharedPreferences.Editor editor
+                String name=text2.getText().toString();
+                String number=text1.getText().toString();
+               /* SharedPreferences.Editor editor
                         =getSharedPreferences("data",MODE_PRIVATE).edit();
                 editor.putString("学号",number);
                 editor.putString("姓名",name);
-                editor.apply();
+                editor.apply(); */
+                Save(name,number);
                 break;
             case R.id.restore_button:
-                SharedPreferences pref
+              /*  SharedPreferences pref
                         =getSharedPreferences("data",MODE_PRIVATE);
                 String names=pref.getString("姓名","无");
                 String numbers=pref.getString("学号","无");
-                String s="姓名："+names+"学号："+numbers;
+                String s="姓名："+names+"\n学号："+numbers;
+                Toast.makeText(MainActivity.this,s,Toast.LENGTH_SHORT).show();
+                */
+                String s=read();
                 Toast.makeText(MainActivity.this,s,Toast.LENGTH_SHORT).show();
             default:
                 break;
+        }
+    }
+
+    private String  read() {
+        FileInputStream in=null;
+        BufferedReader reader=null;
+        StringBuilder content=new StringBuilder();
+        try{
+            in=openFileInput("data");
+            reader=new BufferedReader(new InputStreamReader(in));
+            String line="";
+            while ((line=reader.readLine())!=null){
+                content.append(line+"   ");
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return  content.toString();
+    }
+
+    private void Save(String name,String number) {
+        FileOutputStream out=null;
+        BufferedWriter writer=null;
+        try{
+            out=openFileOutput("data", Context.MODE_PRIVATE);
+            writer=new BufferedWriter(new OutputStreamWriter(out));
+            writer.write("学号："+number+"\n姓名："+name);
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            try{
+                if(writer!=null){
+                    writer.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
         }
     }
 
